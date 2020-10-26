0b1cf9dd967f7d18fcb39998022e53d207357aea
Alex Astakhov <madmonkey@mail.ru>
2020-10-27 00:49:49 +0300

    edit simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,19 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    {
-        let map = diskomap::BTree::open_or_create("db/simple_db.txt", None)?;
-        map.insert(0, "Masha".to_string())?;
-        map.insert(1, "Sasha".to_string())?;
-        map.insert(0, "Natasha".to_string())?;
-    }
+    let map = diskomap::BTree::open_or_create("db/simple_db.txt", None)?;
+    map.insert(0, "Masha".to_string())?;
+    map.insert(1, "Sasha".to_string())?;
+    map.insert(0, "Natasha".to_string())?;
+    drop(map);
 
-    // reload
-    {
-        let map = diskomap::BTree::open_or_create("db/simple_db.txt", None)?;
-        assert_eq!(map.get(&0)?, Some("Natasha".to_string()));
-        assert_eq!(map.get(&1)?, Some("Sasha".to_string()));
-        map.remove(&0)?;
-        map.remove(&1)?;
-    }
+    // open in next time
+    let map = diskomap::BTree::open_or_create("db/simple_db.txt", None)?;
+    assert_eq!(map.get(&0)?, Some("Natasha".to_string()));
+    assert_eq!(map.get(&1)?, Some("Sasha".to_string()));
+    map.remove(&0)?;
+    map.remove(&1)?;
 
     Ok(())
 }
