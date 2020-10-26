a07f133615f8573b54261b32ce276e62040ac564
Alex Astakhov <madmonkey@mail.ru>
2020-10-27 00:12:10 +0300

    edit integrity example

--- examples/integrity.rs
+++ examples/integrity.rs
@@ -14,9 +14,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // For unchanged integrity, you can use the sha256 chain, where each line will contain
     // the sum of the hash of the previous line with the data hash of the current line.
 
-    let blockchain_file = "db/blockchain.txt";
     // The initial hash to be used as the previous line hash for generate hash first line.
     let initial_hash = String::new();
+    let blockchain_file = "db/blockchain.txt";
     let users = diskomap::BTree::open_or_create(blockchain_file, Some(diskomap::Integrity::Sha256Blockchain(initial_hash)))?;
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
