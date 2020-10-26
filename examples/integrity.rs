f9eeb88287714a04da9c096d2911d661c4d8203f
Alex Astakhov <madmonkey@mail.ru>
2020-10-27 00:11:46 +0300

    edit integrity example

--- examples/integrity.rs
+++ examples/integrity.rs
@@ -1,19 +1,29 @@
+use std::fs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // For simple data integrity in the log file, you can use a crc32 checksum for each line.
-    let users = diskomap::BTree::open_or_create("db/integrity_crc32.txt", Some(diskomap::Integrity::Crc32))?;
+    let file_with_crc32 = "db/integrity_crc32.txt";
+    let users = diskomap::BTree::open_or_create(file_with_crc32, Some(diskomap::Integrity::Crc32))?;
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
     users.insert(5, "c".to_string())?;
+    drop(users);
+    println!("File content with crc32:");
+    print!("{}", fs::read_to_string(file_with_crc32)?);
 
     // For unchanged integrity, you can use the sha256 chain, where each line will contain
     // the sum of the hash of the previous line with the data hash of the current line.
 
+    let blockchain_file = "db/blockchain.txt";
     // The initial hash to be used as the previous line hash for generate hash first line.
     let initial_hash = String::new();
-    let users = diskomap::BTree::open_or_create("db/blockchain.txt", Some(diskomap::Integrity::Sha256Blockchain(initial_hash)))?;
+    let users = diskomap::BTree::open_or_create(blockchain_file, Some(diskomap::Integrity::Sha256Blockchain(initial_hash)))?;
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
     users.insert(5, "c".to_string())?;
+    drop(users);
+    println!("Blockchain file content:");
+    print!("{}", fs::read_to_string(blockchain_file)?);
 
     Ok(())
 }
