d8c444d5e8fa338be9b5827b04c4008e6a9be2cb
Alex Astakhov <madmonkey@mail.ru>
2020-10-27 00:00:44 +0300

    fix comments in example

--- examples/integrity.rs
+++ examples/integrity.rs
@@ -1,16 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // For simple data integrity on disk, you can use crc32 for each line in log file.
+    // For simple data integrity in the log file, you can use a crc32 checksum for each line.
     let users = diskomap::BTree::open_or_create("db/integrity_crc32.txt", Some(diskomap::Integrity::Crc32))?;
 
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
     users.insert(5, "c".to_string())?;
 
-    // For for irreplaceable integrity, you can use sha256 chain, where each line
-    // will contain the hash of the continuation of the hash of the previous line
-    // with the hash of the data of the current line.
+    // For unchanged integrity, you can use the sha256 chain, where each line will contain
+    // the sum of the hash of the previous line with the data hash of the current line.
 
-    // initial_hash will use as previous hash of first block
+    // The initial hash to be used as the previous line hash for generate hash first line.
     let initial_hash = String::new();
     let users = diskomap::BTree::open_or_create("db/blockchain.txt", Some(diskomap::Integrity::Sha256Blockchain(initial_hash)))?;
 
