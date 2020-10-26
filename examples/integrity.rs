3757812a457ea5ebcc6a97c5fb51dad15d8f464a
Alex Astakhov <madmonkey@mail.ru>
2020-10-27 00:01:54 +0300

    formatting in example

--- examples/integrity.rs
+++ examples/integrity.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // For simple data integrity in the log file, you can use a crc32 checksum for each line.
     let users = diskomap::BTree::open_or_create("db/integrity_crc32.txt", Some(diskomap::Integrity::Crc32))?;
-
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
     users.insert(5, "c".to_string())?;
@@ -12,7 +11,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // The initial hash to be used as the previous line hash for generate hash first line.
     let initial_hash = String::new();
     let users = diskomap::BTree::open_or_create("db/blockchain.txt", Some(diskomap::Integrity::Sha256Blockchain(initial_hash)))?;
-
     users.insert(0, "a".to_string())?;
     users.insert(3, "b".to_string())?;
     users.insert(5, "c".to_string())?;
