7bc41cc9680133f96263535dd4d0b22c677dcd76
AlekseyAstakhov <twinsen@tutanota.com>
2020-11-10 13:20:35 +0300

    comments

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,4 +1,4 @@
-/// Config.
+/// Config of map with storing.
 #[derive(Clone)]
 pub struct Cfg {
     /// Mechanism of controlling the integrity of stored data in a log file.
@@ -8,14 +8,15 @@ pub struct Cfg {
 /// Mechanism of controlling the integrity of stored data in a log file.
 #[derive(Clone)]
 pub enum Integrity {
-    // For Sha256 blockchain. Each line in the operations log file will contain
-    // the sum of the hash of the previous line with the operation + data hash of the current line.
+    /// For Sha256 blockchain. Each line in the operations log file will contain
+    /// the sum of the hash of the previous line with the operation + data hash of the current line.
     Sha256Chain(String),
-    // crc32 (ieee) checksum of operation and data for each line in the operations log file.
+    /// crc32 (ieee) checksum of operation and data for each line in the operations log file.
     Crc32,
 }
 
 impl Default for Cfg {
+    /// Default config of map with storing.
     fn default() -> Self {
         Cfg {
             integrity: None,
